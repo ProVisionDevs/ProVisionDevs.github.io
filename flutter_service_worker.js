26845738b943767f139394f95570bcfa0415ad06
ProVision Devs <provisiondevs@gmail.com>
2023-03-05 02:44:31 -0600

    New fix attempt
    Take 99 lol

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e8966c0a244c96a9c4dad0ba06d4d895",
-"index.html": "4a0813ca93a6dac94c738f4a284b1334",
-"/": "4a0813ca93a6dac94c738f4a284b1334",
-"main.dart.js": "e62bec54ae837235be1f146b40fcb8be",
+"index.html": "e35af1a3cad94486c2d1b88117ce0aa1",
+"/": "e35af1a3cad94486c2d1b88117ce0aa1",
+"main.dart.js": "9256c85068c13f535bcb5d5454d4a7be",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "02f0dad34c32ddcc852fce81d7f3defe",
 "icons/Icon-192.png": "f6388f431375707a7b4dd6f92b9359a9",
